979191ccfbfd61d2c1f29c476ed963007ada45cf
sunaaank <suna.namgung@gmail.com>
2021-03-09 16:31:16 +0900

    Change : Refactor Footer 컴포넌트에 Array.map() 적용

--- src/Pages/suna/Main/MainRight/Footer/Footer.js
+++ src/Pages/suna/Main/MainRight/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Footer.scss';
+import { INFO } from "./footerData";
 
 class Footer extends React.Component {
   render() {
@@ -7,17 +8,13 @@ class Footer extends React.Component {
       <div> 
         <footer className="footer">
           <div className="insta-footer">
-            <a href=".">소개</a>
-            <a href=".">도움말</a>
-            <a href=".">홍보 센터</a>
-            <a href=".">API</a>
-            <a href=".">채용 정보</a>
-            <a href=".">개인정보처리방침</a>
-            <a href=".">약관</a>
-            <a href=".">위치</a>
-            <a href=".">인기 계정</a>
-            <a href=".">해시태그</a>
-            <a href=".">언어</a>
+            {INFO.map(el => {
+              return (
+                <li key={el.id}>
+                  <a href="/">{el.content}</a>
+                </li>
+              )
+            })}
           </div>
           <span>© 2021 WESTAGRAM <br />FROM WECODE 18th @sunaaank</span>
         </footer>
